29f16053b18ecbfe08912b37e2491d94a0de4e8f
Hasrivastav <harshsrivastav2111@gmail.com>
2024-11-15 09:56:33 +0530

    production commit third api Documentation

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
             {routes.map((route, index) => (
               <Route key={index} path={route.path} element={route.element} />
             ))}
-           
+            <Route path="*" element={<docs />} />
           </Routes>
         </div>
       </AuthProvider>
